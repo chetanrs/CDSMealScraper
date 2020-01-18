d188ce32d3e94b146ebc17c812d62c6e0d3d40c6
chetanrs@live.unc.edu <chetanrs@live.unc.edu>
2020-01-18 17:48:54 -0500

    Added database functionality (csv)

--- main.py
+++ main.py
@@ -12,41 +12,47 @@ driver.get("https://dining.unc.edu/locations/top-of-lenoir/?date=2020-" + month 
 
 links = driver.find_elements_by_class_name("show-nutrition")
 
-items_to_traverse = 5
+items_to_traverse = 50
 item_count = 0
-nutritional_labels = ["Calories: ",
-                      "Calories from fat: ",
-                      "Total Fat(g): ",
-                      "Saturated Fat(g): ",
-                      "Trans Fat(g): ",
-                      "Cholesterol(mg): ",
-                      "Sodium(mg): ",
-                      "Total Carbohydrate(g): ",
-                      "Dietary Fiber(g): ",
-                      "Sugars(g): ",
-                      "Protein(g): "]
+nutritional_labels = ["Item",
+                      "Calories",
+                      "Calories From Fat",
+                      "Total Fat (g)",
+                      "Saturated Fat (g)",
+                      "Trans Fat (g)",
+                      "Cholesterol (mg)",
+                      "Sodium (mg)",
+                      "Total Carbohydrate (g)",
+                      "Dietary Fiber (g)",
+                      "Sugars (g)",
+                      "Protein (g)"]
 
-for link in links:
-    if not link.is_displayed() or item_count == items_to_traverse:
-        break
+with open('items.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(nutritional_labels)
 
-    link.click()
+    for link in links:
+        if not link.is_displayed() or item_count == items_to_traverse:
+            break
 
-    try:
-        nutrition = driver.find_element_by_id("nutrition-slider-stage").find_element_by_css_selector("div").text.splitlines()
-    except NoSuchElementException:
-        time.sleep(0.1)
+        link.click()
 
-    print("Item: " + nutrition[0])
-    for i in range(5, 16):
-        for num in nutrition[i].split():
-            try:
-                print(nutritional_labels[i - 5] + str(float(num)))
-            except ValueError:
-                pass
+        try:
+            nutrition = driver.find_element_by_id("nutrition-slider-stage").find_element_by_css_selector("div").text.splitlines()
+        except NoSuchElementException:
+            time.sleep(0.1)
 
-    driver.find_element_by_class_name("close-nutrition").click()
-    item_count += 1
-    print("")
+        row_to_append = [nutrition[0]]
+        for i in range(5, 16):
+            for num in nutrition[i].split():
+                try:
+                    row_to_append.append(float(num))
+                except ValueError:
+                    pass
+
+        writer.writerow(row_to_append)
+
+        driver.find_element_by_class_name("close-nutrition").click()
+        item_count += 1
 
 driver.quit()
